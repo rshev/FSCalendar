30e07c1480a2dc7eaccb73d4d9dafac9437fe62a
Roman Shevtsov <roman.shevtsov@gmail.com>
2021-01-13 21:15:05 +0000

    Adds tvOS 9 support to Package.swift.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,10 @@ import PackageDescription
 
 let package = Package(
     name: "FSCalendar",
-    platforms: [.iOS(.v8)],
+    platforms: [
+        .iOS(.v8),
+        .tvOS(.v9),
+    ],
     products: [
         .library(
             name: "FSCalendar",
